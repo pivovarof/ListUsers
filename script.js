c85884d843dd4136f4bbcd2876e8070081dfd55e
Alex <lgagrog@gmail.com>
2022-01-29 10:08:21 +0200

    test

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const addBtn = document.getElementById('addBtn');
 const tbody = document.getElementById('tbody');
 const saveBtn = document.getElementsByClassName('saveBtn');
 
+
 let arrUsers;
 !localStorage.arrUsers ? arrUsers = [] : arrUsers = JSON.parse(localStorage.getItem('arrUsers'));
+
 let idRandom = () => Math.floor(Math.random() * 10000);
-
-
 
 function addLeadZero(d) {
     return (d < 10) ? '0' + d : d;
@@ -21,10 +21,10 @@ function addDateUsers(t = new Date()) {
 
 }
 
-function UserAdd() {
+function UserAdd(name, email) {
     this.id = idRandom();
-    this.name = '',
-    this.email = '',
+    this.name = name,
+    this.email = email,
     this.date = addDateUsers();
 
 }
@@ -33,8 +33,7 @@ const updatetLocal = () => {
     localStorage.setItem('arrUsers', JSON.stringify(arrUsers));
 }
 
-addBtn.addEventListener('click', () => {
-    console.log('1');
+addBtn.addEventListener('click', () => {    
     arrUsers.push(new UserAdd());
     updatetLocal();
     fillUsersList();    
@@ -45,16 +44,16 @@ function addObjUsers() {
     arrUsers.push(user)
     
 }
-
+let inInput = 'type="text" class="name" name="name"'
 
 const createUsersList = (user, index) => { 
       
      return(`<tr>
      <td>
-         <input type="text" name="name">
+         <input ${inInput}>
      </td>
      <td>
-         <input type="email" name="email">
+         <input type="email" class="email" name="email">
      </td>
      <td>${user.date}</td>
      <td>
@@ -68,7 +67,7 @@ const createUsersList = (user, index) => {
          </button>
      </td>
      <td>
-         <button class="saveBtn activeBtn">Save</button>
+            <button class="saveBtn activeBtn" type="submit" onclick="saveUser(${index})">Save</button>
          <button class="confBtn">Confirm</button>
      </td>
  </tr>`)
@@ -85,7 +84,18 @@ const fillUsersList = () => {
     }
 }
 
-console.log(arrUsers);
+const saveUser = i => {
+    const inputName = document.querySelectorAll('.name')
+    const inputEmail = document.querySelectorAll('.email')
+    
+    arrUsers.push(new UserAdd(inputName[i].value, inputEmail[i].value))
+    inInput = `type="text" class="name" name="name" readonly value=${inputName[i].value}`
+    updatetLocal();
+    fillUsersList();
+    // arrUsers[i].name = inputName[i].value
+    // arrUsers[i].email = inputEmail[i].value
+    console.log(inputName[i].attributes = inInput);
+}
 
 
 
